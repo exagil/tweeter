d0624b83be860b7b120148be385644c415040731
Sumit Das <sumitdas.usit@gmail.com>
2015-11-11 14:15:11 +0530

    Added toast messages to profile clicks on tweet

--- app/src/main/java/twitter/sumit/com/twitterapp/TweetActivity.java
+++ app/src/main/java/twitter/sumit/com/twitterapp/TweetActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,6 +17,9 @@ public class TweetActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        openProfiles("Retweet Guy!", R.id.rt_block);
+        openProfiles("Tweet owner!", R.id.tweet_block);
+
         buttonAction("Reply, baby!", R.id.tweet_reply_btn);
         buttonAction("Retweet, baby!", R.id.tweet_rt_btn);
         buttonAction("Favorite, baby!", R.id.tweet_fav_btn);
@@ -25,6 +29,18 @@ public class TweetActivity extends AppCompatActivity {
         TextView button;
         button = (TextView) findViewById(id);
         button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), message,
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void openProfiles(final String message, int id) {
+        LinearLayout area;
+        area = (LinearLayout) findViewById(id);
+        area.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), message,

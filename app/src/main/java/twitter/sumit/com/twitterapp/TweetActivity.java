e962e7c8db509c5764b059c55488001faa4d40b7
Sumit Das <sumitdas.usit@gmail.com>
2015-11-11 14:06:03 +0530

    Added Toast messages to reply, rt and favorite buttons

--- app/src/main/java/twitter/sumit/com/twitterapp/TweetActivity.java
+++ app/src/main/java/twitter/sumit/com/twitterapp/TweetActivity.java
@@ -1,11 +1,11 @@
 package twitter.sumit.com.twitterapp;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class TweetActivity extends AppCompatActivity {
 
@@ -15,6 +15,22 @@ public class TweetActivity extends AppCompatActivity {
         setContentView(R.layout.activity_tweet);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        buttonAction("Reply, baby!", R.id.tweet_reply_btn);
+        buttonAction("Retweet, baby!", R.id.tweet_rt_btn);
+        buttonAction("Favorite, baby!", R.id.tweet_fav_btn);
+    }
+
+    private void buttonAction(final String message, int id) {
+        TextView button;
+        button = (TextView) findViewById(id);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), message,
+                        Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
 }
